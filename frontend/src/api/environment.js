f8cc2beb88f60d7160c7f4462a86cb97e73ee4b6
Jordan <jordan.terrazos@gmail.com>
2022-03-11 20:35:50 -0500

    presentacion

--- frontend/src/api/environment.js
+++ frontend/src/api/environment.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const URL_BACKEND = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
-  // baseURL: 'http://34.238.235.4:8000',
+  // baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'http://34.238.235.4:8000',
   headers: {
     'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
     'Content-Type': 'application/json',
